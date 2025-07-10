82c1161e97974e6cf55ea2d9f8907637c519d7c9
Muhammed Mashood Alungal <muhdmashoodalungal@gmail.com>
2025-07-10 11:39:51 +0530

    chore: Project dockerized

--- client-a/src/app.module.ts
+++ client-a/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagingModule } from './messaging/messaging.module';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
-  imports: [],
+  imports: [MessagingModule, RabbitmqModule],
   controllers: [AppController],
   providers: [AppService],
 })
